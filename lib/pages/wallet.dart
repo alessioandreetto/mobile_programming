a8141546bfac9f2560332769836e5f66e5669a71
Porpo46 <73351245+Porpo46@users.noreply.github.com>
2024-05-28 11:06:51 +0200

    aggiornato eliminazione wallet e swipe grafici

--- lib/pages/wallet.dart
+++ lib/pages/wallet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 import '../model/database_model.dart';
 import 'new_wallet.dart';
@@ -46,35 +47,37 @@ class _WalletPageState extends State<WalletPage> {
   void initState() {
     super.initState();
     Provider.of<WalletProvider>(context, listen: false).loadValuta();
-    // Non è necessario chiamare _loadNotes() qui, lo chiameremo in didChangeDependencies().
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    // Chiamiamo _loadNotes() qui per essere sicuri che venga chiamato quando il Provider notifica i cambiamenti.
     _loadNotes();
   }
 
-
   void _loadNotes() async {
-        List<Wallet> wallets = await Provider.of<WalletProvider>(context, listen: false).loadWallets();
+    List<Wallet> wallets =
+        await Provider.of<WalletProvider>(context, listen: false).loadWallets();
 
     setState(() {
-      data = wallets.map((wallet) => Note(
-        id: wallet.id,
-        title: wallet.balance!,
-        body: wallet.name!,
-      )).toList();
+      data = wallets
+          .map((wallet) => Note(
+                id: wallet.id,
+                title: wallet.balance!,
+                body: wallet.name!,
+              ))
+          .toList();
     });
   }
 
   void _saveNotes() async {
-    List<Wallet> wallets = data.map((note) => Wallet(
-      id: note.id,
-      name: note.body,
-      balance: note.title,
-    )).toList();
+    List<Wallet> wallets = data
+        .map((note) => Wallet(
+              id: note.id,
+              name: note.body,
+              balance: note.title,
+            ))
+        .toList();
     for (var wallet in wallets) {
       if (wallet.id == null) {
         await DatabaseHelper().insertWallet(wallet);
@@ -91,8 +94,8 @@ class _WalletPageState extends State<WalletPage> {
     required String body,
   }) async {
     if (index >= 0 && index < data.length) {
-      // Update existing wallet
-      Wallet existingWallet = await DatabaseHelper().getWalletById(data[index].id!);
+      Wallet existingWallet =
+          await DatabaseHelper().getWalletById(data[index].id!);
       Wallet updatedWallet = Wallet(
         id: existingWallet.id,
         name: body,
@@ -108,7 +111,6 @@ class _WalletPageState extends State<WalletPage> {
       });
       Provider.of<WalletProvider>(context, listen: false).loadWallets();
     } else {
-      // Insert new wallet
       Wallet newWallet = Wallet(
         name: body,
         balance: title,
@@ -132,36 +134,20 @@ class _WalletPageState extends State<WalletPage> {
       data.removeAt(index);
     });
     Provider.of<WalletProvider>(context, listen: false).loadWallets();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Wallet eliminato')),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Expanded(
-              child: Text(
-                'Wallets',
-                textAlign: TextAlign.start,
-              ),
-            ),
-          ],
-        ),
-        elevation: 0,
-        actions: [
-          if (selectedIndices.isNotEmpty)
-            IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                // Implement delete functionality
-              },
-            ),
-        ],
+        title: Text('Wallets'),
       ),
- body: Consumer<WalletProvider>(
+      body: Consumer<WalletProvider>(
         builder: (context, walletProvider, _) {
-  String valuta = walletProvider.valuta;
+          String valuta = walletProvider.valuta;
 
           return Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -177,55 +163,56 @@ class _WalletPageState extends State<WalletPage> {
                   crossAxisCount: 2,
                   children: walletProvider.wallets.map((wallet) {
                     final index = walletProvider.wallets.indexOf(wallet);
-                    return buildItem(context, index, wallet.balance!, wallet.name!, valuta);
+                    return buildItem(
+                        context, index, wallet.balance!, wallet.name!, valuta);
                   }).toList(),
                   onReorder: (oldIndex, newIndex) {
                     // Implement reorder functionality
                   },
-              footer: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AddNotePage(
-                          onSave: (title, body) {
-                            addOrEditNote(
-                              context: context,
-                              index: -1,
-                              title: title,
-                              body: body,
-                            );
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 0,
-                    margin: EdgeInsets.all(0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xffb3b3b3),
-                            width: 1,
+                  footer: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddNotePage(
+                              onSave: (title, body) {
+                                addOrEditNote(
+                                  context: context,
+                                  index: -1,
+                                  title: title,
+                                  body: body,
+                                );
+                                Navigator.pop(context);
+                              },
+                            ),
                           ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 0,
+                        margin: EdgeInsets.all(0),
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.add,
-                              color: Color(0xff262626),
-                              size: 50,
-                       ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Color(0xffb3b3b3),
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.add,
+                                  color: Color(0xff262626),
+                                  size: 50,
+                                ),
                               ],
                             ),
                           ),
@@ -242,20 +229,21 @@ class _WalletPageState extends State<WalletPage> {
     );
   }
 
-  Widget buildItem(BuildContext context, int index, double title, String body, String valuta) {
+  Widget buildItem(BuildContext context, int index, double title, String body,
+      String valuta) {
     final isSelected = selectedIndices.contains(index);
 
     return GestureDetector(
-      //per eliminazione multipla di wallet, manca la funzione del db 
       onLongPress: () {
-/*         setState(() {
+        HapticFeedback.vibrate();
+        setState(() {
           if (isSelected) {
             selectedIndices.remove(index);
           } else {
             selectedIndices.add(index);
           }
-        }); */
-      }, 
+        });
+      },
       onTap: () {
         if (selectedIndices.isNotEmpty) {
           setState(() {
@@ -266,84 +254,138 @@ class _WalletPageState extends State<WalletPage> {
             }
           });
         } else {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddNotePage(
-                onSave: (newTitle, newBody) {
-                  addOrEditNote(
-                    context: context,
-                    index: index,
-                    title: newTitle,
-                    body: newBody,
-                  );
-                  Navigator.pop(context); // Chiudi AddNotePage dopo il salvataggio
-                },
-                initialTitle: title,
-                initialBody: body,
-                onDelete: () {
-                  deleteNote(index); // Chiama deleteNote quando viene attivato onDelete
-                  Navigator.pop(context); // Chiudi AddNotePage dopo la cancellazione
-                },
+          // Verifica se l'elemento è selezionato per la modifica o per la creazione di un nuovo wallet
+          if (index == data.length) {
+            // Toccare l'elemento per creare un nuovo wallet
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddNotePage(
+                  onSave: (newTitle, newBody) {
+                    addOrEditNote(
+                      context: context,
+                      index: index,
+                      title: newTitle,
+                      body: newBody,
+                    );
+                    Navigator.pop(context);
+                  },
+                  initialTitle: title,
+                  initialBody: body,
+                  onDelete: () {
+                    deleteNote(index);
+                    Navigator.pop(context);
+                  },
+                ),
               ),
-            ),
-          );
+            );
+          } else {
+            // Toccare l'elemento per modificare un wallet esistente
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddNotePage(
+                  onSave: (newTitle, newBody) {
+                    addOrEditNote(
+                      context: context,
+                      index: index,
+                      title: newTitle,
+                      body: newBody,
+                    );
+                    Navigator.pop(context);
+                  },
+                  initialTitle: title,
+                  initialBody: body,
+                  onDelete: () {
+                    deleteNote(index);
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            );
+          }
         }
       },
       key: ValueKey(index),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: isSelected ? Colors.red : Color(0xffb3b3b3),
-              width: 1,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: isSelected ? Colors.red : Color(0xffb3b3b3),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        body,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontFamily: 'RobotoThin',
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Balance: ",
+                            style: TextStyle(
+                              fontFamily: 'RobotoThin',
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "$title $valuta",
+                            style: TextStyle(
+                              fontFamily: 'RobotoThin',
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    body,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontFamily: 'RobotoThin',
-                      fontSize: 40,
+            if (isSelected)
+              Positioned(
+                top: 0,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () {
+                    deleteNote(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                    padding: EdgeInsets.all(4),
+                    child: Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                      size: 20,
                     ),
                   ),
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Balance: ",
-                        style: TextStyle(
-                          fontFamily: 'RobotoThin',
-                          fontSize: 10,
-                        ),
-                      ),
-                      Text(
-                        "$title $valuta",
-                        style: TextStyle(
-                          fontFamily: 'RobotoThin',
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
