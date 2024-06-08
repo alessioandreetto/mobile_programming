d9f4edf5190449d877f66eb829826f685a3b25bc
alessioandreetto <72388010+alessioandreetto@users.noreply.github.com>
2024-06-08 13:17:02 +0200

    filtro entrate e uscite

--- lib/sliverbar_test.dart
+++ lib/sliverbar_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../providers/wallet_provider.dart';
 import 'package:provider/provider.dart'; // Assicurati di importare correttamente il pacchetto provider
 import '../model/database_model.dart'; // Assicurati di importare correttamente la tua classe di gestione del database
+import 'dart:math' as Math;
 
 void main() {
   runApp(MyApp());
@@ -27,6 +28,7 @@ class SliverTest extends StatefulWidget {
 class _SliverTestState extends State<SliverTest> {
   late List<Transaction> transactions = [];
   late int _selectedWalletIndex = 0; // Assicurati di dichiarare correttamente _selectedWalletIndex
+  bool _showExpenses = true;
 
   @override
   void initState() {
@@ -34,17 +36,31 @@ class _SliverTestState extends State<SliverTest> {
     _loadTransactions(_selectedWalletIndex); // Carica le transazioni del portafoglio iniziale
   }
 
-  Future<void> _loadTransactions(int walletIndex) async {
-    try {
-      List<Transaction> loadedTransactions =
-          await DatabaseHelper().getTransactionsForWallet(walletIndex + 1); // Aggiungi 1 perché gli indici iniziano da 0
-      setState(() {
-        transactions = loadedTransactions;
-      });
-    } catch (e) {
-      print('Errore durante il caricamento delle transazioni: $e');
+Future<void> _loadTransactions(int walletIndex) async {
+  try {
+    List<Transaction> loadedTransactions = await DatabaseHelper()
+        .getTransactionsForWallet(walletIndex + 1); // Aggiungi 1 perché gli indici iniziano da 0
+    
+    if (_showExpenses) {
+      // Filtra solo le transazioni di uscita
+      loadedTransactions = loadedTransactions
+          .where((transaction) => transaction.value! < 0)
+          .toList();
+    } else {
+      // Filtra solo le transazioni di entrata
+      loadedTransactions = loadedTransactions
+          .where((transaction) => transaction.value! >= 0)
+          .toList();
     }
+
+    setState(() {
+      transactions = loadedTransactions;
+    });
+  } catch (e) {
+    print('Errore durante il caricamento delle transazioni: $e');
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -98,53 +114,86 @@ class _SliverTestState extends State<SliverTest> {
           SliverPersistentHeader(
             pinned: true,
             delegate: _SliverPersistentHeaderDelegate(
-              minHeight: 50.0,
-              maxHeight: 50.0,
+              
+              minHeight: 100.0,
+              maxHeight: 100.0,
               child: Container(
-                height: 50,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: walletProvider.wallets.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _selectedWalletIndex = index;
-                          });
-                          _loadTransactions(index); // Carica le transazioni del nuovo portafoglio selezionato
-                        },
-                        style: ButtonStyle(
-                          elevation: MaterialStateProperty.all(0),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                            side: BorderSide(color: Colors.black),
-                          )),
-                          foregroundColor:
-                              MaterialStateProperty.resolveWith<Color>(
-                            (Set<MaterialState> states) {
-                              return _selectedWalletIndex == index
-                                  ? Colors.white
-                                  : Colors.black;
-                            },
-                          ),
-                          backgroundColor:
-                              MaterialStateProperty.resolveWith<Color>(
-                            (Set<MaterialState> states) {
-                              return _selectedWalletIndex == index
-                                  ? Colors.black
-                                  : Colors.white;
-                            },
-                          ),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Transazioni per ${walletProvider.wallets[_selectedWalletIndex].name}:"),
+                    DropdownButton<bool>(
+                      value: _showExpenses,
+                      onChanged: (value) {
+                        setState(() {
+                          _showExpenses = value!;
+                          _loadTransactions(_selectedWalletIndex);
+                        });
+                      },
+                      items: [
+                        DropdownMenuItem<bool>(
+                          value: true,
+                          child: Text('Mostra Uscite'),
                         ),
-                        child: Text(walletProvider.wallets[index].name!),
+                        DropdownMenuItem<bool>(
+                          value: false,
+                          child: Text('Mostra Entrate'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                    Container(
+                      height: 50,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: walletProvider.wallets.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _selectedWalletIndex = index;
+                                });
+                                _loadTransactions(index); // Carica le transazioni del nuovo portafoglio selezionato
+                              },
+                              style: ButtonStyle(
+                                elevation: MaterialStateProperty.all(0),
+                                shape:
+                                    MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  side: BorderSide(color: Colors.black),
+                                )),
+                                foregroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    return _selectedWalletIndex == index
+                                        ? Colors.white
+                                        : Colors.black;
+                                  },
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    return _selectedWalletIndex == index
+                                        ? Colors.black
+                                        : Colors.white;
+                                  },
+                                ),
+                              ),
+                              child: Text(walletProvider.wallets[index].name!),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -171,6 +220,7 @@ class _SliverTestState extends State<SliverTest> {
               childCount: transactions.length, // Numero di transazioni
             ),
           ),
+          
         ],
       ),
     );
@@ -210,3 +260,4 @@ class _SliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
         child != oldDelegate.child;
   }
 }
+
