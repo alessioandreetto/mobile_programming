27b1c02333b2263944ae2145d61fede4eda6e6ab
alessioandreetto <72388010+alessioandreetto@users.noreply.github.com>
2024-06-08 12:04:29 +0200

    test sliver

---
+++ lib/sliverbar_test.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SliverTest(),
+    );
+  }
+}
+
+class SliverTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            snap: true,
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Portafoglio',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                Text(
+                  'di',
+                  style: TextStyle(fontSize: 12.0),
+                ),
+                Text(
+                  'Utente',
+                  style: TextStyle(fontSize: 12.0),
+                ),
+              ],
+            ),
+            floating: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _SliverPersistentHeaderDelegate(
+              minHeight: 50.0,
+              maxHeight: 50.0,
+              child: Container(
+                color: Colors.green,
+                alignment: Alignment.center,
+                child: Text('Portafoglio'),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100.0,
+                    child: Center(child: Text('Elemento $index')),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0xffb3b3b3),
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                );
+              },
+              childCount: 20, // Numero di elementi nella lista
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  _SliverPersistentHeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverPersistentHeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
