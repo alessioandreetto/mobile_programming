eb0a4f86a0f7d591980a671a2aff96ca1d914e0e
Porpo46 <73351245+Porpo46@users.noreply.github.com>
2024-06-14 09:58:04 +0200

    sistemato nome wallet tutorial

--- lib/pages/initialPage/welcome_page.dart
+++ lib/pages/initialPage/welcome_page.dart
@@ -2,17 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/wallet_provider.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  late TextEditingController _nameController;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late PageController _pageController;
+  bool _isButtonDisabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _pageController = PageController();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final provider = Provider.of<WalletProvider>(context, listen: false);
+      if (provider.name != 'User') {
+        _nameController.text = provider.name;
+        _isButtonDisabled = _nameController.text.isEmpty;
+      }
+    });
+
+    _nameController.addListener(() {
+      setState(() {
+        _isButtonDisabled = _nameController.text.isEmpty;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onSwipe(int pageIndex) {
+    if (_formKey.currentState!.validate()) {
+      _pageController.animateToPage(
+        pageIndex,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.ease,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: PageView(
+          controller: _pageController,
+          physics: _isButtonDisabled
+              ? NeverScrollableScrollPhysics()
+              : AlwaysScrollableScrollPhysics(),
           children: [
-            Expanded(
+            Form(
+              key: _formKey,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
@@ -24,36 +77,51 @@ class WelcomePage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20.0),
-                  Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Inserisci il tuo nome per iniziare',
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (value) {
-                          // Ottieni un'istanza di WalletProvider e aggiorna il nome dell'account
-                          Provider.of<WalletProvider>(context, listen: false)
-                              .updateAccountName(value);
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      Text(
-                        'Con questa app semplice e intuitiva, '
-                        'potrai tenere traccia delle tue spese quotidiane '
-                        'e pianificare un futuro finanziario migliore. '
-                        'Inizia ad esplorare il mondo delle tue finanze oggi stesso!',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Inserisci il tuo nome per iniziare',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Inserire il nome';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      Provider.of<WalletProvider>(context, listen: false)
+                          .updateAccountName(value);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Con questa app semplice e intuitiva, '
+                    'potrai tenere traccia delle tue spese quotidiane '
+                    'e pianificare un futuro finanziario migliore. '
+                    'Inizia ad esplorare il mondo delle tue finanze oggi stesso!',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ],
               ),
             ),
+            // Aggiungi altre pagine qui
+            Center(
+              child: Text('Pagina successiva'),
+            ),
           ],
+          onPageChanged: (index) {
+            if (index == 1 && _isButtonDisabled) {
+              _pageController.animateToPage(
+                0,
+                duration: Duration(milliseconds: 300),
+                curve: Curves.ease,
+              );
+            }
+          },
         ),
       ),
     );
